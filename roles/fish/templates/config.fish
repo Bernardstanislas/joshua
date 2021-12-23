6a5bcfb51983bf9e5ac4a5f9ed3a56c5f6041996
Stanislas Bernard <bernardstanislas@gmail.com>
2021-12-23 20:36:06 +0100

    :bug: Fix fish startup script

--- roles/fish/templates/config.fish
+++ roles/fish/templates/config.fish
@@ -1,5 +1,4 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    [ -f /usr/share/autojump/autojump.fish ]; and source /usr/share/autojump/autojump.fish
 end
-
-[ -f /usr/share/autojump/autojump.fish ];
