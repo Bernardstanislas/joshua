b09b013fe6985b6b2e9ffd46a30644cdc4054b89
Stanislas Bernard <bernardstanislas@gmail.com>
2022-01-04 12:10:04 +0100

    :sparkles: Do not log journal entries

--- roles/fish/templates/config.fish
+++ roles/fish/templates/config.fish
@@ -2,3 +2,6 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     [ -f /usr/share/autojump/autojump.fish ]; and source /usr/share/autojump/autojump.fish
 end
+
+alias doco=docker-compose
+abbr --add jrnl " jrnl"
