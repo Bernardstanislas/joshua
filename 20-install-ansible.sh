f22e528c63bed4333871ea8e5d2adb5f809b9cad
Stanislas Bernard <bernardstanislas@gmail.com>
2022-01-30 23:01:44 +0100

    :construction: Transform provisioning into ubuntu

--- 20-install-ansible.sh
+++ 20-install-ansible.sh
@@ -2,4 +2,3 @@ sudo apt install software-properties-common
 sudo add-apt-repository --yes --update ppa:ansible/ansible
 sudo apt install ansible
 which ansible || yay -S --noconfirm ansible
-ansible-galaxy collection install -r requirements.yml
